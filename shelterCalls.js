c5813a2a87521ae99fdb22cec6e066705a4ab3a2
Maritza Alas <alas.maritza.j@gmail.com>
2017-12-09 21:51:36 -0800

    initial commit

---
+++ shelterCalls.js
@@ -0,0 +1,12 @@
+var shelterGet = function (onSuccess, onError) {
+    var url = "http://5a2c8607d38d3c0012a094c9.mockapi.io/shelter/1";
+    var settings = {
+        cache: false,
+        contentType: "application/x-www-form-urlencoded; charset=UTF-8",
+        dataType: "json",
+        success: onSuccess,
+        error: onError,
+        type: "GET"
+    }
+    $.ajax(url, settings);
+}
